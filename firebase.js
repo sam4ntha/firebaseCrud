f08b78b78f7cac9c15d7ce9437bd5951fa302e34
sam4ntha <stap0428@gmail.com>
2022-11-21 17:38:33 -0600

    Actualizar datos

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, onSnapshot, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, onSnapshot, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,3 +29,6 @@ export const onGetTasks = (callback) => onSnapshot(collection(db, 'tasks'), call
 export const deleteTask = id =>  deleteDoc(doc(db, 'tasks', id))
 
 export const getTask = id => getDoc(doc(db, 'tasks', id))
+
+export const updateTask = (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);
+
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import {saveTask, getTasks, onGetTasks, deleteTask, getTask} from './firebase.js'
+import {saveTask, getTasks, onGetTasks, deleteTask, getTask, updateTask} from './firebase.js'
 const taskForm = document.getElementById('task-form')
 const tasksContainer = document.getElementById('tasks-container')
 let editStatus = false;
+let id = '';
 
 window.addEventListener('DOMContentLoaded', async () => {
     
@@ -38,6 +39,9 @@ window.addEventListener('DOMContentLoaded', async () => {
                 taskForm['task-description'].value = task.description
 
                 editStatus = true;
+                id = doc.id
+
+                taskForm['btn-task-save'].innerText = 'Update'
             })
         })
     });
@@ -50,10 +54,12 @@ taskForm.addEventListener('submit', (e) => {
     const title = taskForm['task-title']
     const description = taskForm['task-description']
 
-    if (editStatus){
-        console.log("Updating");
-    } else {
+    if (!editStatus){
         saveTask(title.value, description.value);
+    } else {
+        updateTask(id,{title: title.value, description: description.value});
+
+        editStatus = false;
     }
     taskForm.reset()
-})
+});
